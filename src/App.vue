59882d64247bf0a799b5565c7601d12750d04e3a
anjiahao <893345820@qq.com>
2022-04-13 16:50:27 +0800

    项目初始化-调整初始目录结构

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    
+    <router-view></router-view>
+    <h1>黑马头条</h1>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style lang="less">
+
+</style>
